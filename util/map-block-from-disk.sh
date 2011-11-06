488deff1f4bd2f6afe0d6b212645939877920816
Marko Aalto <marko@Macintosh-3.local>
2011-11-06 14:30:07 +0200

    updated example to use starting map block 101

--- util/map-block-from-disk.sh
+++ util/map-block-from-disk.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #  View map block from disk
-#  Example: ./map-block-from-disk.sh ../c64u4/ULTIMA4C.D64 35
+#  Example, starting location at Lord British castle:
+#     ./map-block-from-disk.sh ../c64u4/ULTIMA4C.D64 101
 
 if [ $# -ne 2 ]; then
   echo "map-block-from-disk.sh <d64-file> <block-number>" 1>&2
@@ -8,7 +9,7 @@ if [ $# -ne 2 ]; then
 fi
 
 FILE=$1
-BLOCK=$2
+BLOCK=$(($2 + 1))
 
 ./extract-bytes $FILE $(($BLOCK*256)) 256 | xxd -g1 | cut -c10-56 | sed 's/00/  /g; s/01/. /g; s/02/../g; s/04/__/g; s/05/ff/g; s/06/HH/g; s/07/MM/g'
 
