a73794fca477e6c98c70ea488f31a9aff8d61f64
Marko Aalto <marko@Macintosh-3.local>
2011-11-06 14:46:01 +0200

    remove debug message

--- util/map-block-from-disk.sh
+++ util/map-block-from-disk.sh
@@ -11,7 +11,6 @@ fi
 FILE=$1
 BLOCK=$(($2 + 1))
 UTIL_DIR=`dirname $0`
-echo $UTIL_DIR
 
 $UTIL_DIR/extract-bytes $FILE $(($BLOCK*256)) 256 | xxd -g1 | cut -c10-56 | sed 's/00/  /g; s/01/. /g; s/02/../g; s/04/__/g; s/05/ff/g; s/06/HH/g; s/07/MM/g; s/0d/!"/g; s/0e/[]/g; s/0f/"!/g; s/0a/To/g'
 
