e866fbe5219acf1db19908fe1b434182c082fe53
Marko Aalto <marko@Macintosh-3.local>
2011-11-06 21:12:49 +0200

    Fixed off by one error. Input parameter is now PETSCII/ASCII code

--- util/charmap-from-disk.sh
+++ util/charmap-from-disk.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #  Read 8 byte char-bitmap from diskimage 
 #  Example for displaying bitmap for letter A
-#     ./charmap-from-disk.sh ../c64u4/ULTIMA4A.D64 $((256*246+65*8-1))
+#     ./charmap-from-disk.sh ../c64u4/ULTIMA4A.D64 65
 
 if [ $# -ne 2 ]; then
-  echo "charmap-from-disk.sh <d64-file> <offset>" 1>&2
+  echo "charmap-from-disk.sh <d64-file> <petscii-code>" 1>&2
   exit 1
 fi
 
 FILE=$1
-OFFSET=$(($2 + 1))
+OFFSET=$((256*246 + $2 * 8))
 
 xxd -l8 -g1 -c1 -b -s $OFFSET $FILE | cut -c10-17 | tr '01' '.*'
