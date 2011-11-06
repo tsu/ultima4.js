3059a07a17d6d85367d5e5b8486516cd2b03f218
Marko Aalto <marko.aalto@iki.fi>
2011-11-06 23:41:08 +0200

    code clean up

--- util/tilemap-from-disk.sh
+++ util/tilemap-from-disk.sh
@@ -14,8 +14,8 @@ OFFSET_2=$((256*359 + $2 * 8 * 2 + 8))
 OFFSET_3=$((256*359 + $2 * 8 * 2 + 4096))
 OFFSET_4=$((256*359 + $2 * 8 * 2 + 4096 + 8))
 
-paste -d' ' <(xxd -l8 -g1 -c1 -b -s $OFFSET_1 $FILE | cut -c10-17 | tr '01' '.\*') \
+paste -d' ' <(xxd -l8 -g1 -c1 -b -s $OFFSET_1 $FILE | cut -c10-17 | tr '01' '.*') \
             <(xxd -l8 -g1 -c1 -b -s $OFFSET_2 $FILE | cut -c10-17 | tr '01' '.*') | tr -d ' '
-paste -d' ' <(xxd -l8 -g1 -c1 -b -s $OFFSET_3 $FILE | cut -c10-17 | tr '01' '.\*') \
+paste -d' ' <(xxd -l8 -g1 -c1 -b -s $OFFSET_3 $FILE | cut -c10-17 | tr '01' '.*') \
             <(xxd -l8 -g1 -c1 -b -s $OFFSET_4 $FILE | cut -c10-17 | tr '01' '.*') | tr -d ' '
 
