4ed4c9c860b29b94e9379d0e6745a13aa403b680
Marko Aalto <marko.aalto@iki.fi>
2011-11-07 23:36:43 +0200

    first working version, which actually creates PNG

--- util/tile-to-png.lisp
+++ util/tile-to-png.lisp
@@ -1,7 +1,14 @@
 #!/usr/local/bin/sbcl --script
 ;;;; Read U4 tile and convert it to PNG  file
 
-;(ql:quickload "zpng")
+;; Quiclisp setup, normally in .sbclrc, which is not read with --script
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(ql:quickload "zpng")
 
 (defun tile-offsets (tile)
   "Convert tile-id into list of offsets into d64-file"
@@ -12,14 +19,33 @@
 	  (+ base 4096)
 	  (+ base 4096 8))))
 
+(defun tile-png (stream tile)
+  "Read tile bitmap from streams and create PNG"
+  (let* ((png (make-instance 'zpng:png :color-type :grayscale
+			    :width 16 :height 16))
+	 (data (zpng:data-array png))
+	 (dest-x 0)
+	 (dest-y 0))
+    (dolist (p (tile-offsets tile))
+      (file-position stream p)
+      (dotimes (y 8)
+	(let ((b (read-byte stream nil)))
+	  (dotimes (x 8)
+	    (setf (aref data (+ dest-y y) (+ dest-x x) 0) 
+		  (if (logbitp (- 7 x) b) 255 0)))))
+      (incf dest-x 8)
+      (when (>= dest-x 16)
+	(incf dest-y 8)
+	(setf dest-x 0)))
+    png))
+
 ;;; Startup from command line
 (let ((argv sb-ext:*posix-argv*))
   (if (> (length argv) 2)
-      (let ((filename (nth 1 argv))
-	    (tile-id (parse-integer (nth 2 argv))))
+      (let* ((filename (nth 1 argv))
+	     (tile-id (parse-integer (nth 2 argv)))
+	     (filename-out (format nil "tile-~D.png" tile-id)))
 	(with-open-file (in filename :element-type '(unsigned-byte 8))
-	  (dolist (p (tile-offsets tile-id))
-	    (file-position in p)
-	    (dotimes (i 8)
-	      (format t "~8,'0B~%" (read-byte in nil))))))
-      (format t "usage: ~A: <d64-file> <tile-id>~%" (car argv))))
+	  (zpng:write-png (tile-png in tile-id) filename-out))
+	(format t "Wrote file ~A~%" filename-out))
+      (format t "usage: tile-to-png.lisp <d64-file> <tile-id>~%")))
