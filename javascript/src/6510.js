3abc193da03a6d21717f1c28040e8eec15f3a783
Timo Suomela <darth.tsu@gmail.com>
2011-11-08 23:09:09 +0200

    Extract function

--- javascript/src/6510.js
+++ javascript/src/6510.js
@@ -15,20 +15,14 @@ var _6510 = (function() {
   function ifTrueSetZ(test) {
      test ? (SR |= flags.Z) : (SR &= (0xff ^ flags.Z));
   }
+  function loadImmediate(r) {
+    return function(v) {
+      ifZeroSetZ(v);
+      r(v);
+    };
+  }
   function ifZeroSetZ(v) {
     ifTrueSetZ(v === 0);
-  }
-  function LDA_i(v) {
-    ifZeroSetZ(v);
-    A = v;
-  }
-  function LDX_i(v) {
-    ifZeroSetZ(v);
-    X = v;
-  }
-  function LDY_i(v) {
-    ifZeroSetZ(v);
-    Y = v;
   }
   function TAX() {
     X = A;
@@ -55,9 +49,9 @@ var _6510 = (function() {
     CMP_i: compareImmediate(function() { return A; }),
     CPX_i: compareImmediate(function() { return X; }),
     CPY_i: compareImmediate(function() { return Y; }),
-    LDA_i: LDA_i,
-    LDX_i: LDX_i,
-    LDY_i: LDY_i,
+    LDA_i: loadImmediate(function(v) { A = v; }),
+    LDX_i: loadImmediate(function(v) { X = v; }),
+    LDY_i: loadImmediate(function(v) { Y = v; }),
     TAX: TAX,
     TAY: TAY,
     TSX: TSX,
