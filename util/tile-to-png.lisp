3a96e1234ae99ffc958f6b1a0f1b27cd4bda71fc
Marko Aalto <marko.aalto@iki.fi>
2011-11-10 00:42:25 +0200

    added creation of world map png file

--- util/tile-to-png.lisp
+++ util/tile-to-png.lisp
@@ -1,5 +1,15 @@
 #!/usr/local/bin/sbcl --script
 ;;;; Read U4 tile and convert it to PNG  file
+;; Examples
+;;
+;; Read and create a single tile
+;;     util/tile-to-png.lisp c64u4/ULTIMA4A.D64 31
+;;
+;; Read and create sequence of 10 tiles starting from 31
+;;     util/tile-to-png.lisp c64u4/ULTIMA4A.D64 31 10
+;;
+;; Read create world map
+;;    util/tile-to-png.lisp c64u4/ULTIMA4A.D64 0 0 c64u4/ULTIMA4C.D64 
 
 ;; Quiclisp setup, normally in .sbclrc, which is not read with --script
 #-quicklisp
@@ -41,43 +51,66 @@
   "Color offset for tile"
   (+ 78000 tile))
 
-(defun blit-byte (byte image-data p colors)
+(defun blit-byte (byte image-data p colors &optional (pitch 3))
   "Copy 8 bits from byte to 8 pixels in array"
   (dotimes (bit 8)
     (let ((rgb (if (logbitp (- 7 bit) byte) 
 		   (nth 1 colors)
 		   (nth 0 colors))))
       (dotimes (i 3)
-	(setf (aref image-data (+ p (* bit 3) i)) 
+	(setf (aref image-data (+ p (* bit pitch) i)) 
 	      (nth i rgb))))))
 
 
-(defun blit-char-cell (cell image-data p colors)
+(defun blit-char-cell (cell image-data p colors &optional (pitch (* 16 3)))
   "Blit char-cell of 8x8 bits into image array"
   (dotimes (i 8)
     (blit-byte (aref cell i) 
 	       image-data 
-	       (+ p (* 16 3 i))
+	       (+ p (* pitch i))
 	       colors)))
 
-(defun tile-png (stream tile)
-  "Read tile bitmap from streams and create PNG"
+(defun blit-tile (tile-data image-data p colors &optional (pitch (* 16 3)))
+  "Blit tile data into image"
+  (let ((png-offsets (list 0 (* 8 3) 
+			   (* 8 pitch) (+ (* 8 pitch) (* 8 3)))))
+    (dotimes (i 4)
+      (blit-char-cell (subseq tile-data (* i 8))
+		      image-data
+		      (+ p (nth i png-offsets))
+		      colors
+		      pitch))))
+  
+(defun read-worldmap (filename)
+  (with-open-file (in filename :element-type '(unsigned-byte 8))
+    (file-position in 256)
+    (let ((map (make-array (* 256 256) :element-type '(unsigned-byte 8))))
+      (dotimes (blk 256)
+	(let* ((bx (mod blk 16))
+	       (by (floor blk 16))
+	       (p (+ (* by 16 256) 
+		     (* bx 16))))
+	  (dotimes (i 16)
+	    (read-sequence map in :start (+ p (* i 256)) :end (+ p (* i 256) 16)))))
+      map)))
+	    
+(defun tile-png (stream tile png &optional (p 0))
+  "Read tile bitmap from streams and write into png-image"
   (file-position stream (color-offset tile))
-  (let* ((png (make-instance 'zpng:png :color-type :truecolor
-			     :width 16 :height 16))
-	 (offsets (tile-offsets tile))
+  (let* ((offsets (tile-offsets tile))
 	 (color-byte (read-byte stream))
 	 (colors (list (aref *palette* (logand color-byte #xF))
 		       (aref *palette* (ash color-byte -4))))
-	 (png-offsets (list 0 (* 8 3) (* 8 16 3) (+ (* 8 16 3) (* 8 3))))
-	 (char-cell (make-array 8 :element-type '(unsigned-byte 8))))
-    (dotimes (i 4)
-      (file-position stream (nth i offsets))
-      (read-sequence char-cell stream)
-      (blit-char-cell char-cell 
-		      (zpng:image-data png) 
-		      (nth i png-offsets)
-		      colors))
+	 (tile-data (make-array (* 4 8) :element-type '(unsigned-byte 8))))
+    (file-position stream (nth 0 offsets))
+    (read-sequence tile-data stream :start 0 :end 16)
+    (file-position stream (nth 2 offsets))
+    (read-sequence tile-data stream :start 16)
+    (blit-tile tile-data
+	       (zpng:image-data png) 
+	       p
+	       colors
+	       (* (zpng:width png) 3))
     png))
   
 ;;; Startup from command line
@@ -87,10 +120,25 @@
 	     (tile-id (parse-integer (nth 2 argv)))
 	     (count (if (> (length argv) 3)
 			(parse-integer (nth 3 argv))
-			1)))
+			1))
+	     (map (if (> (length argv) 4)
+		      (nth 4 argv))))
 	(with-open-file (in filename :element-type '(unsigned-byte 8))
-	  (dotimes (i count)
-	    (let ((filename-out (format nil "tile-~2,'0X.png" (+ tile-id i))))
-	      (zpng:write-png (tile-png in (+ tile-id i)) filename-out)
-	      (format t "Wrote file ~A~%" filename-out)))))
-      (format t "usage: tile-to-png.lisp <d64-file> <tile-id> [<count>=1]~%")))
+	  (if map
+	      (let ((png (make-instance 'zpng:png :color-type :truecolor
+					:width (* 256 16) :height (* 256 16)))
+		    (map (read-worldmap map)))
+		(dotimes (y 256)
+		  (dotimes (x 256)
+		    (let ((tile (aref map (+ (* y 256) x))))
+		      (tile-png in tile png (+ (* y 256 16 16 3) (* x 16 3))))))
+		(zpng:write-png png "map.png")
+		(format t "Wrote file map.png~%"))
+	      (dotimes (i count)
+		(let ((filename-out (format nil "tile-~2,'0X.png" (+ tile-id i)))
+		      (png (make-instance 'zpng:png :color-type :truecolor
+					  :width 16 :height 16)))
+		  (tile-png in (+ tile-id i) png)
+		  (zpng:write-png png filename-out)
+		  (format t "Wrote file ~A~%" filename-out))))))
+      (format t "usage: tile-to-png.lisp <d64-file> <tile-id> [<count>=1] [<d64-map-disk>]~%")))
