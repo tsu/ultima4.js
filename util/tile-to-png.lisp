ad093d8c9a9dec5a14fda57503ab93c3f33b0cb2
Marko Aalto <marko.aalto@iki.fi>
2011-11-11 00:15:46 +0200

    Code cleanup. Less loops, some maps

--- util/tile-to-png.lisp
+++ util/tile-to-png.lisp
@@ -21,84 +21,69 @@
 (ql:quickload "zpng")
 
 ;; Default palette of C64 colors used by VICE
-(defvar *palette* #((#x00 #x00 #x00)     ; 0 black
-		    (#xFD #xFE #xFC)     ; 1 white
-		    (#xBE #x1A #x24)     ; 2 red
-		    (#x30 #xE6 #xC6)     ; 3 cyan
-		    (#xB4 #x1A #xE2)     ; 4 purple
-		    (#x1F #xD2 #x1E)     ; 5 green
-		    (#x21 #x1B #xAE)     ; 6 blue
-		    (#xDF #xF6 #x0A)     ; 7 yellow
-		    (#xB8 #x41 #x04)     ; 8 orange
-		    (#x6A #x33 #x04)     ; 9 brown
-		    (#xFE #x4A #x57)     ; A light red
-		    (#x42 #x45 #x40)     ; B dark grey
-		    (#x70 #x74 #x6F)     ; C grey
-		    (#x59 #xFE #x59)     ; D light green
-		    (#x5F #x53 #xFE)     ; E light blue
-		    (#xA4 #xA7 #xA2)))   ; F light grey
+(defvar *palette* #(#(#x00 #x00 #x00)     ; 0 black
+		    #(#xFD #xFE #xFC)     ; 1 white
+		    #(#xBE #x1A #x24)     ; 2 red
+		    #(#x30 #xE6 #xC6)     ; 3 cyan
+		    #(#xB4 #x1A #xE2)     ; 4 purple
+		    #(#x1F #xD2 #x1E)     ; 5 green
+		    #(#x21 #x1B #xAE)     ; 6 blue
+		    #(#xDF #xF6 #x0A)     ; 7 yellow
+		    #(#xB8 #x41 #x04)     ; 8 orange
+		    #(#x6A #x33 #x04)     ; 9 brown
+		    #(#xFE #x4A #x57)     ; A light red
+		    #(#x42 #x45 #x40)     ; B dark grey
+		    #(#x70 #x74 #x6F)     ; C grey
+		    #(#x59 #xFE #x59)     ; D light green
+		    #(#x5F #x53 #xFE)     ; E light blue
+		    #(#xA4 #xA7 #xA2)))   ; F light grey
 
-(defun tile-offsets (tile)
-  "Convert tile-id into list of offsets into d64-file"
-  (let ((base (+ (* 256 359)
-		 (* tile 8 2))))
-    (list base 
-	  (+ base 8)
-	  (+ base 4096)
-	  (+ base 4096 8))))
-
-(defun color-offset (tile)
-  "Color offset for tile"
-  (+ 78000 tile))
+(defvar *tile-offset-top-row*    (* #x100 #x167))
+(defvar *tile-offset-bottom-row* (+ *tile-offset-top-row* #x1000))
+(defvar *tile-offset-color*      78000)
 
 (defun blit-byte (byte png x y colors)
-  "Copy 8 bits from byte to 8 pixels in array"
-  (let ((image-data (zpng:image-data png))
-	(pitch (zpng:rowstride png)))
-    (dotimes (bit 8)
-      (let ((rgb (nth (if (logbitp (- 7 bit) byte)
-			  1
-			  0)
-		      colors))
-	    (p (+ (* y pitch) (* (+ x bit) (zpng:samples-per-pixel png)))))
-	(dotimes (i 3)
-	  (setf (aref image-data (+ p i))
-		(nth i rgb)))))))
-
+  "Copy 8 bits from byte to 8 pixels in png"
+  (dotimes (bit 8)
+    (replace (zpng:image-data png)
+	     (if (logbitp (- 7 bit) byte)
+		 (nth 1 colors)
+		 (nth 0 colors))
+	     :start1 (+ (* y (zpng:rowstride png))
+			(* (+ x bit) (zpng:samples-per-pixel png))))))
 
 (defun blit-char-cell (cell png x y colors)
   "Blit char-cell of 8x8 bits into image array"
   (dotimes (i 8)
-    (blit-byte (aref cell i) png
-	       x (+ y i) colors)))
-
+    (blit-byte (aref cell i) png x (+ y i) colors)))
 
 (defun blit-tile (tile-data png x y colors)
   "Blit tile data into image"
-  (blit-char-cell (subseq tile-data (* 0 8)) png x y colors)
-  (blit-char-cell (subseq tile-data (* 1 8)) png (+ x 8) y colors)
-  (blit-char-cell (subseq tile-data (* 2 8)) png x (+ y 8) colors)
-  (blit-char-cell (subseq tile-data (* 3 8)) png (+ x 8) (+ y 8) colors))
+  (mapc #'(lambda (i x y) (blit-char-cell (subseq tile-data (* i 8)) 
+					  png x y colors))
+	(list 0 1 2 3)
+	(list x (+ x 8) x (+ x 8))
+	(list y y (+ y 8) (+ y 8))))
 
-  
+(defun tile-colors (color-byte)
+  "Convert tile-core byte into list of RGB values"
+  (list (aref *palette* (logand color-byte #xF))
+	(aref *palette* (ash color-byte -4))))
 	    
 (defun tile-png (stream tile png &optional (x 0) (y 0))
   "Read tile bitmap from streams and write into png-image"
-  (file-position stream (color-offset tile))
-  (let* ((offsets (tile-offsets tile))
-	 (color-byte (read-byte stream))
-	 (colors (list (aref *palette* (logand color-byte #xF))
-		       (aref *palette* (ash color-byte -4))))
+  (file-position stream (+ *tile-offset-color* tile))
+  (let* ((color-byte (read-byte stream))
 	 (tile-data (make-array (* 4 8) :element-type '(unsigned-byte 8))))
-    (file-position stream (nth 0 offsets))
+    (file-position stream (+ *tile-offset-top-row* (* tile 2 8)))
     (read-sequence tile-data stream :start 0 :end 16)
-    (file-position stream (nth 2 offsets))
+    (file-position stream (+ *tile-offset-bottom-row* (* tile 2 8)))
     (read-sequence tile-data stream :start 16)
-    (blit-tile tile-data png
-	       x y colors)
+    (blit-tile tile-data png x y (tile-colors color-byte))
     png))
 
 (defun read-worldmap (filename)
+  "Read worl map (tile codes) from file into array"
   (with-open-file (in filename :element-type '(unsigned-byte 8))
     (file-position in 256)
     (let ((map (make-array (* 256 256) :element-type '(unsigned-byte 8))))
