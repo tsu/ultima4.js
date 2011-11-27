9de652b959524fbe047dd09d773074f892fff21d
Marko Aalto <marko.aalto@iki.fi>
2011-11-27 15:33:23 +0200

    few more ascii tricks

--- util/town-map-from-disk.sh
+++ util/town-map-from-disk.sh
@@ -13,6 +13,6 @@ BLOCK=$(($2 + 0))
 UTIL_DIR=`dirname $0`
 
 $UTIL_DIR/extract-bytes $FILE $(($BLOCK*256)) 1024 | xxd -g1 -c32 | cut -c10-104 | \
-    sed 's/7f/##/g; s/04/__/g; s/05/ff/g; s/06/FF/g; s/3e/,./g; s/3b/||/g; s/7d/[]/g; s/30/()/g; s/02/../g; s/3f/,,/g; s/16/--/g; s/33/\/\//g; s/34/\\\\/g; s/48/XX/g; s/49/%%/g; s/3c/$$/g'  | \
+    sed 's/7f/##/g; s/04/__/g; s/05/ff/g; s/06/FF/g; s/3e/,./g; s/3b/||/g; s/7d/[]/g; s/30/()/g; s/02/../g; s/3f/,,/g; s/16/--/g; s/33/\/\//g; s/34/\\\\/g; s/48/XX/g; s/49/%%/g; s/3c/$$/g; s/7a/==/g; s/7b/==/g; s/7c/==/g; s/47/-_/g; s/3d/o+/g' | \
     tr -d ' ' | sed 's/00/  /g; s/01/. /g'
 
