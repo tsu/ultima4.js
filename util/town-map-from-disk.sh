eae516ad108232e209fbcb347a2493500c762f0f
Marko Aalto <marko.aalto@iki.fi>
2011-11-27 14:35:16 +0200

    added some ascii art to town maps

--- util/town-map-from-disk.sh
+++ util/town-map-from-disk.sh
@@ -12,5 +12,6 @@ FILE=$1
 BLOCK=$(($2 + 0))
 UTIL_DIR=`dirname $0`
 
-$UTIL_DIR/extract-bytes $FILE $(($BLOCK*256)) 1024 | xxd -g1 -c32 | cut -c10-104
+$UTIL_DIR/extract-bytes $FILE $(($BLOCK*256)) 1024 | xxd -g1 -c32 | cut -c10-104 | \
+    sed 's/7f/##/g; s/04/__/g; s/05/ff/g; s/3e/,./g; s/3b/||/g; s/7d/[]/g; s/30/()/g; s/02/../g; s/3f/,,/g; s/16/--/g'
 
